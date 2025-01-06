da684615d9b3dc8a711a8a39ab6a30f4232e3271
x-Lo <christoslordopoulos@hotmail.com>
2025-01-06 17:40:58 +0200

    minor styling changes

--- src/components/DailyWeight.vue
+++ src/components/DailyWeight.vue
@@ -142,13 +142,13 @@ export default defineComponent({
 
 <style scoped>
 .dailyweight-page {
-  height: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   text-align: center;
-  gap: 3rem;
+  
   padding: 1rem;
 }
 
